2222ba6ca2dce4143e6d5c61754b29a56eee0e61
AliHaider332 <bhaialihaider332@gmail.com>
2025-09-18 19:24:24 +0500

    Update the code and manage uploding using buffers

--- mini-linkdin/BackEnd/index.js
+++ mini-linkdin/BackEnd/index.js
@@ -55,6 +55,12 @@ app.use(
     resave: false,
     saveUninitialized: false,
     store: store,
+    cookie: {
+      httpOnly: true,
+      secure: process.env.NODE_ENV === 'production', // true for Vercel
+      sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'lax',
+      maxAge: 1000 * 60 * 60 * 24, // 1 day
+    },
   })
 );
 
